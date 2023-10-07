31abb634e0663d992c1206ebe9262274112d21e0
Sihyeon0123 <snc100194@gmail.com>
2023-10-07 20:45:33 +0900

    메인화면 수정
    한글 출력 테스트를 위한 폰트 추가 및 출력 추가

--- main_frame.py
+++ main_frame.py
@@ -1,38 +1,48 @@
-# 메인화면을 보여줄 소스코드입니다.
+# 메인화면(시작화면)을 보여줄 소스코드입니다.
 # 현재에는 아무 기능도 구현되지 않았습니다.
 # 작성자: 양시현
 # 수정 이력:
 # - 2023-09-24: 초기버전 생성
+# - 2023-10-07: 한글 출력 테스트를 위한 폰트 추가 및 출력 추가
 
 import pygame
 import sys
 from pygame.locals import *
-import tetris
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+#import tetirs
+
 
 def main():
     pygame.init()
-    pygame.display.set_caption("Pygame에서 한국어 표시하기")
+    pygame.display.set_caption("메인화면")
     screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
-    font = pygame.font.SysFont("malgungothic", 80) # 시스템 폰트 사용 시
-    tmr = 0
+    # 폰트 생성
+    font = pygame.font.SysFont("malgungothic", 80)
 
     while True:
         key = None
         for event in pygame.event.get():
-            if event.type == QUIT: # 이벤트 타입이 종료면 게임 종료
+            # 이벤트 타입이 QUIT이면 프로그램 종료
+            if event.type == QUIT: 
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN: # 키를 눌렀을때 만약 esc키라면 종료
                 key = event.key
                 if key == K_ESCAPE:
                     pygame.display.set_mode((600, 600))
-                    tetris.main('AI')
+                    #tetris.main('AI')
+                    
+        # 점수 자리수 설정
+        score_str = "한글 출력"
+        # 스코어 폰트 및 색상 설정
+        score_image = font.render(score_str, True, (180, 180, 180))
+        # 점수 출력 자리 설정
+        screen.blit(score_image, (100, 100))
 
+        # 화면 업데이트 
+        pygame.display.update()
+    
 
-        
 if __name__ == '__main__':
     main()
